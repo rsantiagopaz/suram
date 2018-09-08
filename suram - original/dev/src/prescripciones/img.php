0d4a3f53161921751edaee6da0d8da5b89052bf4
rsantiagopaz <rsantiagopaz@gmail.com>
2018-09-08 16:05:51 -0300

    x

--- suram - original/dev/src/prescripciones/img.php
+++
@@ -1,8 +0,0 @@
-<?php
-include("../control_acceso_flex.php");
-include($SYSpathraiz.'phpqrcode/phpqrcode.php');
-
-$texto = $_GET['texto'];
-
-QRcode::png($texto);
-?>
--- suram - original/dev/src/prescripciones/qrcode.php
+++
@@ -1,8 +0,0 @@
-<?php
-include("../control_acceso_flex.php");
-include($SYSpathraiz.'phpqrcode/phpqrcode.php');
-
-$texto = "aa";
-
-QRcode::png($texto,'codigo.png');
-?>
